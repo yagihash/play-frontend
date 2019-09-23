42995dd73d2d7271e3ba8de980cd0552eafcfc22
Yu YAGIHASHI <yagihash@xss.moe>
2019-09-23 20:38:39 +0900

    add starter kit

---
+++ app/actions/TopActionCreators.js
@@ -0,0 +1,25 @@
+import AppDispatcher from "../dispatcher/AppDispatcher";
+import { ActionTypes } from "../constants/AppConstants";
+
+const getTopAction001 = () => ({
+  type: ActionTypes.TOP_TYPE_001,
+  data: {
+    title: "New Title",
+    subtitle: "Created by ActionCreator",
+    text: "This text will be overwritten"
+  }
+});
+
+const getTopAction002 = () => ({
+  type: ActionTypes.TOP_TYPE_002,
+  data: "RESULT OF YOUT ACTION"
+});
+
+export const TopActionCreators = {
+  actionCreator001() {
+    AppDispatcher.dispatch(getTopAction001());
+  },
+  actionCreator002() {
+    AppDispatcher.dispatch(getTopAction002());
+  }
+};
